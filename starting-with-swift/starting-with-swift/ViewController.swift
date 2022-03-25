2ea451dd30801164bf88cb6fcd7443e584167e91
Pedro Bueno <pedroarcbueno@gmail.com>
2022-03-25 09:54:26 -0300

    First aura class

---
+++ starting-with-swift/starting-with-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  starting-with-swift
+//
+//  Created by Pedro Bueno on 25/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
